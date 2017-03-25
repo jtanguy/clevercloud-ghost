a8f34459b75ce3c8ca1cdbd763973584fe8919ae
Julien Tanguy <jtanguy@valwin.fr>
2017-03-25 12:38:36 +0100

    Hardcode the keybase url directly

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const ghost = require('ghost');
 const express = require('express');
+const path = require('path');
 
 const wrapperApp = express();
 
 ghost({
   config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
-    wrapperApp.use('/.well-known', express.static('well-known'));
+    wrapperApp.get('/.well-known/keybase.txt', function(req, res, next) {
+        res.sendFile(path.join(__dirname, 'well-known', 'keybase.txt'));
+    });
     wrapperApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+
     ghostServer.start(wrapperApp);
 });
